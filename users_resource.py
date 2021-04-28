bac06c61649a950f4e9e0ad5346f56fc3844ce31
Gertrude-s <66856406+Gertrude-s@users.noreply.github.com>
2021-04-28 09:06:20 +0700

    Update users_resource.py

--- users_resource.py
+++ users_resource.py
@@ -17,7 +17,7 @@ class UserResource(Resource):
         abort_if_user_not_found(user_id)
         session = db_session.create_session()
         user = session.query(User).get(user_id)
-        return jsonify({'user': user.to_dict()})
+        return jsonify({'user': user.to_dict(only=('id', 'nickname', 'email', 'country', 'age'))})
 
     def delete(self, user_id):
         abort_if_user_not_found(user_id)
@@ -31,8 +31,8 @@ class UserResource(Resource):
 class UsersListResource(Resource):
     def get(self):
         session = db_session.create_session()
-        news = session.query(User).all()
-        return jsonify({'users': [item.to_dict() for item in news]})
+        users = session.query(User).all()
+        return jsonify({'users': [item.to_dict(only=('id', 'nickname', 'email', 'country', 'age')) for item in users if item]})
 
     def post(self):
         args = user_parser.parse_args()
